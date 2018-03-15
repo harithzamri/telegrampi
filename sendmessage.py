f8ab2d1857ecf4e79e67603367c9ce906c87b9c9
harithzamri <37389526+harithzamri@users.noreply.github.com>
2018-03-15 09:41:14 +0800

    Add files via upload

---
+++ sendmessage.py
@@ -0,0 +1,26 @@
+import os
+import re
+import sys
+import datetime
+from datetime import timedelta
+import json
+import subprocess
+import MySQLdb
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import telepot
+
+
+def sendmsg():
+    print("your temperature is in danger")
+    
+bot=telepot.Bot('Bot Token')
+bot.message_loop(handle)
+print ('im listening')
+
+while 1:
+    time.sleep(0)
+     
+        
+
